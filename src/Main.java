98c24e060c6e89c27686ac5e9366c6f40d3c3b1a
Denis <denisfilipov.12@gmail.com>
2019-11-06 21:22:27 +0100

    added a new class and called its methods from our main package

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import data.Person;
+
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
+        Person person = new Person("Pepe");
+
+        String name = person.getName();
+
+        System.out.println(name);
     }
 }
---
+++ src/data/Person.java
@@ -0,0 +1,13 @@
+package data;
+
+public class Person {
+    private String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
